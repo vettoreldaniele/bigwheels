00786623775d3fc53cecc25ea0b6d6be5913a552
Mark Segal <segal@google.com>
2021-02-08 11:43:55 -0800

    Allocation test

--- src/ppx/grfx/grfx_buffer.cpp
+++ src/ppx/grfx/grfx_buffer.cpp
@@ -5,6 +5,7 @@ namespace grfx {
 
 Result Buffer::Create(const grfx::BufferCreateInfo* pCreateInfo)
 {
+#if 0
     // Constant/uniform buffers need to be at least PPX_CONSTANT_BUFFER_ALIGNMENT in size
     if (pCreateInfo->usageFlags.bits.uniformBuffer && (pCreateInfo->size < PPX_CONSTANT_BUFFER_ALIGNMENT)) {
         PPX_ASSERT_MSG(false, "constant/uniform buffer sizes must be at least PPX_CONSTANT_BUFFER_ALIGNMENT (" << PPX_CONSTANT_BUFFER_ALIGNMENT << ")");
@@ -16,7 +17,7 @@ Result Buffer::Create(const grfx::BufferCreateInfo* pCreateInfo)
         PPX_ASSERT_MSG(false, "storage/structured buffer sizes must be at least PPX_STUCTURED_BUFFER_ALIGNMENT (" << PPX_STUCTURED_BUFFER_ALIGNMENT << ")");
         return ppx::ERROR_GRFX_MINIMUM_BUFFER_SIZE_NOT_MET;
     }
-
+#endif
     Result ppxres = grfx::DeviceObject<grfx::BufferCreateInfo>::Create(pCreateInfo);
     if (Failed(ppxres)) {
         return ppxres;
