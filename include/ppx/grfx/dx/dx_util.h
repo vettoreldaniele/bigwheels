8627cb3f4708f0f8298bf990dd139aef6434bf1e
Hai Nguyen <haing@google.com>
2021-03-09 18:10:13 -0500

    Moved ToDxgiFormat to common dx namespace

---
+++ include/ppx/grfx/dx/dx_util.h
@@ -0,0 +1,18 @@
+#ifndef ppx_grfx_dx_util_h
+#define ppx_grfx_dx_util_h
+
+#include "ppx/grfx/grfx_format.h"
+
+#include <dxgi1_6.h>
+
+namespace ppx {
+namespace grfx {
+namespace dx {
+
+DXGI_FORMAT ToDxgiFormat(grfx::Format value);
+
+} // namespace dx
+} // namespace grfx
+} // namespace ppx
+
+#endif ppx_grfx_dx_util_h
