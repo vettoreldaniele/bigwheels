238242f24000d147d5fcbb370490a9e986055369
Diego Novillo <dnovillo@google.com>
2022-10-12 12:23:32 -0400

    Add Apache license headers to all the source files. (#228)
    * Add Apache license headers to all the source files.
    
    This adds the Apache license header to all files with suffix *.hlsl,
    *.hlsli, *.cpp, *.h in the directories assets/, benchmarks/, include/,
    projects/ third_party/imgui_extra and src/

--- src/ppx/grfx/vk/vk_profiler_fn_wrapper.h
+++ src/ppx/grfx/vk/vk_profiler_fn_wrapper.h
@@ -1,3 +1,17 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef PPX_GRFX_VK_PROFILER_FN_WRAPPHER_H
 #define PPX_GRFX_VK_PROFILER_FN_WRAPPHER_H
 
